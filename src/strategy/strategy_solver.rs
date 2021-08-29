66f0eddbe9e757608aefb9300178dbd5abfa3387
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-08-29 22:44:20 +0200

    Pass Sudoku into StrategySolver

--- src/strategy/strategy_solver.rs
+++ src/strategy/strategy_solver.rs
@@ -5,23 +5,25 @@ use super::{Strategy, algorithms::NakedSingle};
 /// The `StrategySolver` is the struct for solving Sudokus
 /// by applying logical strategies that humans can do.
 pub struct StrategySolver {
+    /// The initial Sudoku
+    sudoku: Sudoku,
     /// List of all strategies
     strategies: Vec<Box<dyn Strategy>>,
 }
 
 impl StrategySolver {
     /// Creates a new Solver with a list of strategies
-    pub fn new() -> Self {
+    pub fn new(sudoku: &Sudoku) -> Self {
         let mut solver = StrategySolver {
+            sudoku: sudoku.clone(),
             strategies: Vec::new(),
         };
-
         solver.add_default_strategies();
         solver
     }
 
     /// Solve the Sudoku by applying solving steps.
-    pub fn solve(&self, _sudoku: &Sudoku) -> Result<(), SolverError> {
+    pub fn solve(&self) -> Result<(), SolverError> {
         Ok(())
     }
 
@@ -47,14 +49,14 @@ mod tests {
     #[test]
     fn solve_sudokus() {
         // A few sudokus found here: https://sandiway.arizona.edu/sudoku/examples.html
-        let solver = StrategySolver::new();
         let sudokus = vec![
             r"...26.7.1 68..7..9. 19...45.. 82.1...4. ..46.29.. .5...3.28 ..93...74 .4..5.36 7.3.18...",
         ];
 
         for s in sudokus {
             let sudoku = Sudoku::try_from(s).unwrap();
-            assert!(solver.solve(&sudoku).is_ok());
+            let solver = StrategySolver::new(&sudoku);
+            assert!(solver.solve().is_ok());
         }
     }
 }

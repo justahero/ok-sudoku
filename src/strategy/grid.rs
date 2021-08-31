aebed7df6a776e2ec71147c16a30cfcf9b45bc46
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-08-31 22:41:48 +0200

    Add functions to Grid struct

--- src/strategy/grid.rs
+++ src/strategy/grid.rs
@@ -1,6 +1,6 @@
-use crate::{Sudoku, Value};
+use crate::{Sudoku, Value, sudoku::GridError, types::{BLOCKS, COLS, HOUSES, Pos, ROWS}};
 
-use super::Cell;
+use super::{Cell, CellState, Digit};
 
 /// Sudoku grid to manipulate with all cells + candidates
 /// The struct is only a helper grid to manipulate during solving.
@@ -33,9 +33,81 @@ impl Grid {
     fn init_candidates(&mut self) {
         self.cells.iter_mut().for_each(|cell| {
             if let Some(candidates) = cell.candidates_mut() {
-                // TODO continue here?
+
             }
         });
+    }
+
+    /// Returns an iterator over all cells
+    pub fn cells(&self) -> impl Iterator<Item = &Cell> {
+        self.cells.iter()
+    }
+
+    /// Returns the cell at coordinates row, col
+    pub fn get(&self, row: u8, col: u8) -> &Cell {
+        let index = col + row * Sudoku::ROWS;
+        &self.cells[index as usize]
+    }
+
+    /// Sets digit to this cell
+    pub fn set(&mut self, row: u8, col: u8, digit: u8) {
+        let index = col + row * Sudoku::ROWS;
+        self.cells[index as usize].state = CellState::Number(Digit::new(digit));
+    }
+
+    /// Naive version to check if Sudoku is solved
+    /// **Note** ignores any checks that each line, row and block contains of digits 1..9
+    pub fn is_solved(&self) -> bool {
+        self.cells.iter().all(|cell| cell.is_digit() )
+    }
+
+    /// Returns all fields for the given row
+    pub fn get_row<'a>(&'a self, row: u8) -> impl Iterator<Item = &Cell> + 'a {
+        let indices = &ROWS[row as usize];
+        indices
+            .iter()
+            .map(move |&index| &self.cells[index as usize])
+    }
+
+    /// Returns all fields for the given column
+    pub fn get_col<'a>(&'a self, col: u8) -> impl Iterator<Item = &Cell> + 'a {
+        let indices = &COLS[col as usize];
+        indices
+            .iter()
+            .map(move |&index| &self.cells[index as usize])
+    }
+
+    /// Returns all fields from the given block
+    pub fn get_block<'a>(&'a self, row: u8, col: u8) -> impl Iterator<Item = &Cell> + 'a {
+        let index = Pos::new(row, col).block();
+        let indices = &BLOCKS[index as usize];
+        indices
+            .iter()
+            .map(move |&index| &self.cells[index as usize])
+    }
+
+    /// Returns the house, all fields from same row, col and block
+    pub fn get_house<'a>(&'a self, row: u8, col: u8) -> impl Iterator<Item = &Cell> + 'a {
+        let index = col + row * Sudoku::ROWS;
+        let indices = &HOUSES[index as usize];
+        indices
+            .iter()
+            .map(move |&index| &self.cells[index as usize])
+    }
+
+    /// Returns the grid as a Sudoku
+    pub fn sudoku(&self) -> Result<Sudoku, GridError> {
+        let numbers = self.cells
+            .iter()
+            .map(|cell| cell.value())
+            .collect::<Vec<_>>();
+        Sudoku::new(numbers)
+    }
+}
+
+impl From<Sudoku> for Grid {
+    fn from(sudoku: Sudoku) -> Self {
+        Grid::new(&sudoku)
     }
 }
 
@@ -47,5 +119,8 @@ impl From<&Sudoku> for Grid {
 
 #[cfg(test)]
 mod tests {
+    #[test]
+    fn test_init_candidates() {
 
+    }
 }

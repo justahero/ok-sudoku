dd92400f5090b783b7d2cd9b64f3d348c7e138c5
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-09-11 23:54:24 +0200

    Start to implement hidden subset strategy
    * add `IndexVec` to group indexes by candidates (inverse from candidates
    by index).

--- src/strategy/algorithms/hidden_subset.rs
+++ src/strategy/algorithms/hidden_subset.rs
@@ -1,4 +1,23 @@
+use std::collections::HashMap;
+
+use bit_vec::BitVec;
+use itertools::Itertools;
+
 use crate::{Cell, Sudoku, strategy::{Strategy, step::Step}};
+
+/// A Bit Vec indicating which indexes a candidate is in.
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct IndexVec(BitVec);
+
+impl IndexVec {
+    pub fn new() -> Self {
+        Self(BitVec::from_elem(82, false))
+    }
+
+    pub fn set(&mut self, index: u8) {
+        self.0.set(index as usize, true);
+    }
+}
 
 #[derive(Debug)]
 pub struct HiddenSubset {
@@ -10,7 +29,26 @@ impl HiddenSubset {
         Self { count: 2 }
     }
 
-    fn find_tuple(_cells: &Vec<&Cell>) -> Option<Step> {
+    fn find_tuple(&self, cells: &Vec<&Cell>) -> Option<Step> {
+        // Map all candidates to cell indexes
+        let candidates = cells.iter().fold(HashMap::new(), |mut map, &cell| {
+            for candidate in cell.candidates_vec() {
+                if map.get(&candidate).is_none() {
+                    map.insert(candidate, IndexVec::new());
+                }
+                if let Some(index) = map.get_mut(&candidate) {
+                    index.set(cell.index() as u8);
+                }
+            }
+            map
+        });
+
+        let group = candidates.iter().permutations(self.count).find(|entries| {
+            entries.iter().map(|(_, indexes)| indexes).all_equal()
+        });
+
+        dbg!(&group);
+
         None
     }
 }
@@ -18,7 +56,19 @@ impl HiddenSubset {
 impl Strategy for HiddenSubset {
     fn find(&self, sudoku: &Sudoku) -> Option<Step> {
         for row in sudoku.get_rows() {
-            if let Some(step) = Self::find_tuple(&row) {
+            if let Some(step) = self.find_tuple(&row) {
+                return Some(step);
+            }
+        }
+
+        for col in sudoku.get_blocks() {
+            if let Some(step) = self.find_tuple(&col) {
+                return Some(step);
+            }
+        }
+
+        for block in sudoku.get_blocks() {
+            if let Some(step) = self.find_tuple(&block) {
                 return Some(step);
             }
         }
@@ -48,6 +98,28 @@ mod tests {
             1......85
             .6..8.271
             ..5.1..94
+        ";
+
+        let mut sudoku = Sudoku::try_from(sudoku).unwrap();
+        sudoku.init_candidates();
+        let strategy = HiddenSubset::pair();
+
+        let step = strategy.find(&sudoku);
+        assert!(step.is_some());
+    }
+
+    #[test]
+    fn find_hidden_subset_pair() {
+        let sudoku = r"
+            .49132...
+            .81479...
+            327685914
+            .96.51.8.
+            .75.28...
+            .38.46..5
+            853267...
+            712894563
+            964513...
         ";
 
         let mut sudoku = Sudoku::try_from(sudoku).unwrap();

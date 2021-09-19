379fa12ca13a4ed18d11a4f31de1886e1f380edc
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-09-19 13:32:15 +0200

    Support locked candidate (pointing tuple) for columns
    * add test

--- src/strategy/algorithms/pointing_tuple.rs
+++ src/strategy/algorithms/pointing_tuple.rs
@@ -1,6 +1,10 @@
 use itertools::Itertools;
 
-use crate::{Sudoku, strategy::{step::Step, Strategy}, types::Index};
+use crate::{
+    strategy::{step::Step, Strategy},
+    types::Index,
+    Sudoku,
+};
 
 pub struct PointingTuple {}
 
@@ -45,7 +49,70 @@ impl Strategy for PointingTuple {
                             let row = Index::from(indexes[0]).row();
                             let eliminates = sudoku
                                 .get_row(row)
-                                .filter(|&cell| cell.has_candidate(candidate) && !indexes.contains(&cell.index()))
+                                .filter(|&cell| {
+                                    cell.has_candidate(candidate)
+                                        && !indexes.contains(&cell.index())
+                                })
+                                .collect_vec();
+
+                            if eliminates.len() > 0 {
+                                let mut step = Step::new();
+
+                                for index in indexes {
+                                    step.lock_candidate(*index, candidate)
+                                }
+                                for neighbor in eliminates {
+                                    step.eliminate_candidate(neighbor.index(), candidate)
+                                }
+
+                                return Some(step);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for cells in sudoku.get_blocks() {
+            let lines = [
+                [&cells[0], &cells[3], &cells[6]],
+                [&cells[1], &cells[4], &cells[7]],
+                [&cells[2], &cells[5], &cells[8]],
+            ];
+
+            for candidate in 1_u8..=9 {
+                // filter cells with the candidate in same group
+                let lines = lines
+                    .iter()
+                    .map(|&group| {
+                        group
+                            .iter()
+                            .filter(|&&cell| cell.has_candidate(candidate))
+                            .map(|&cell| cell.index())
+                            .collect_vec()
+                    })
+                    .filter(|group| !group.is_empty())
+                    .collect_vec();
+
+                for i in 0..=lines.len() {
+                    if let Some((&indexes, others)) = lines
+                        .iter()
+                        .cycle()
+                        .skip(i)
+                        .take(lines.len())
+                        .collect_vec()
+                        .split_first()
+                    {
+                        if indexes.len() >= 2 && others.len() == 0_usize {
+                            let col = Index::from(indexes[0]).col();
+
+                            // get remaining cells from same column
+                            let eliminates = sudoku
+                                .get_col(col)
+                                .filter(|&cell| {
+                                    cell.has_candidate(candidate)
+                                        && !indexes.contains(&cell.index())
+                                })
                                 .collect_vec();
 
                             if eliminates.len() > 0 {
@@ -109,8 +176,30 @@ mod tests {
         assert_eq!(&vec![(24_usize, 5)], step.eliminated_candidates());
     }
 
+    // Example: https://www.free-sudoku-puzzle.net/techniques/locked-candidate-pointing?example=3
     #[test]
     fn pointing_tuple_in_col_found() {
-        todo!();
+        let sudoku = r"
+            957842136
+            .1.97....
+            ..41...9.
+            64.59..12
+            ....61...
+            .91..86..
+            38..194..
+            4.9.8..21
+            1....4.6.
+        ";
+
+        let mut sudoku = Sudoku::try_from(sudoku).unwrap();
+        sudoku.init_candidates();
+        // remove a few candidates to reflect linked Sudoku example exactly
+        sudoku.get_mut(68).unset_candidate(6);
+        sudoku.get_mut(38).unset_candidate(5);
+        let strategy = PointingTuple::new();
+
+        let step = strategy.find(&sudoku).expect("Should return step");
+        assert_eq!(&vec![(64_usize, 7), (73_usize, 7)], step.locked_candidates(),);
+        assert_eq!(&vec![(37_usize, 7)], step.eliminated_candidates());
     }
 }
